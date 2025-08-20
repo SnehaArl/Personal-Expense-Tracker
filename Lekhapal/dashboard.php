df6cb22b38e6a620286f2f95a05695bdf8baccaf
SnehaArl <sneha17bit22@kcc.edu.np>
2025-08-20 15:56:49 +0545

    Update dashboard.php

--- Lekhapal/dashboard.php
+++ Lekhapal/dashboard.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />  
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
 
 <body>
@@ -22,16 +22,31 @@
         <main id="dashboard-main">
             <div class="top-bar">
                 <span id="menu" class="material-symbols-sharp" onclick='openSidebar()'>menu</span>
-              
-        <form method="get" action="">
-        <label>Display:</label>
-        <select name="filter" onchange="this.form.submit()">
-            <option value="current" <?php if(!isset($_GET['filter']) || $_GET['filter']=='all') echo 'selected'; ?>>Current</option>
-            <option value="month" <?php if(isset($_GET['filter']) && $_GET['filter']=='month') echo 'selected'; ?>>This Month</option>
-            <option value="week" <?php if(isset($_GET['filter']) && $_GET['filter']=='week') echo 'selected'; ?>>This Week</option>
-        </select>
-        </form>
+
+                <form method="get" action="">
+                    <label>Display:</label>
+                    <select name="filter" onchange="this.form.submit()">
+                        <option value="today" <?php if (!isset($_GET['filter']) || $_GET['filter'] == 'today') echo 'selected'; ?>>Today</option>
+                        <option value="month" <?php if (isset($_GET['filter']) && $_GET['filter'] == 'month') echo 'selected'; ?>>This Month</option>
+                        <option value="week" <?php if (isset($_GET['filter']) && $_GET['filter'] == 'week') echo 'selected'; ?>>This Week</option>
+                    </select>
+                </form>
             </div>
+
+            <?php
+            $filter = $_GET['filter'] ?? 'current'; //if exists uses its value else default: current
+
+            $condition = "";
+            if ($filter == "today") {
+                $condition = "AND DATE(date) = CURDATE()";
+            }
+            if ($filter == "week") {
+                $condition = "AND YEARWEEK(date) = YEARWEEK(CURDATE())";
+            } elseif ($filter == "month") {
+                $condition = "AND YEAR(date) = YEAR(CURDATE()) AND MONTH(date) = MONTH(CURDATE())";
+            }
+            ?>
+
 
             <div class="content1">
                 <h2>Wallet</h2>
@@ -75,9 +90,9 @@
                     }
                     ?>
                 </div>
-                </div>
-                <div class="content4" style="background-color: #ffffff;color:#000;">Image
-                </div>
+            </div>
+            <div class="content4" style="background-color: #ffffff;color:#000;">Image
+            </div>
         </main>
     </div>
 </body>
