5f3c33015b90788634db6ad7bd679e74b742f0fd
SnehaArl <sneha17bit22@kcc.edu.np>
2025-08-20 14:29:43 +0545

    Update dashboard.php

--- Lekhapal/dashboard.php
+++ Lekhapal/dashboard.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <style>
+   <!-- <style>
         #dashboard-main {
             background-color: var(--clr-background);
             grid-area: main;
@@ -76,7 +76,7 @@
         .content4 {
             grid-area: content4;
         }
-    </style> 
+    </style> -->
 </head>
 
 <body>
