424bfd6c3cbbed522288662dbb690c3655e422dc
SnehaArl <sneha17bit22@kcc.edu.np>
2025-08-29 08:56:23 +0545

    sx

--- Lekhapal/contact.php
+++ Lekhapal/contact.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    // User not logged in, redirect to login
+    header("Location: login.php");
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
